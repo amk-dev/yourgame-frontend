b5b1c4532301786fe6eff460627e8e99e96d0365
amk-dev <akash.k.mohan98@gmail.com>
2021-03-08 04:45:12 +0530

    fix netlify build variable name

--- src/services/YourGameApi.js
+++ src/services/YourGameApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 let baseURL = 'http://localhost:3000'
 
-if (process.env.context == 'staging') {
+if (process.env.CONTEXT == 'branch-deploy') {
 	baseURL = 'https://staging.api.yourgame.live'
 }
 
