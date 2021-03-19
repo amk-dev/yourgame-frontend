e97198ad1b42e7feb3c1fed86550669ef84a35d1
amk-dev <akash.k.mohan98@gmail.com>
2021-03-19 22:16:34 +0530

    Remove Unused CSS using purgecss

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,42 @@
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 // 	.BundleAnalyzerPlugin
 
-// module.exports = {
-// 	configureWebpack: {
-// 		plugins: [new BundleAnalyzerPlugin()],
-// 	},
-// }
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+module.exports = {
+	// configureWebpack: {
+	// 	plugins: [new BundleAnalyzerPlugin()],
+	// },
+	css: {
+		loaderOptions: {
+			postcss: {
+				plugins: [
+					purgecss({
+						content: [`./public/**/*.html`, `./src/**/*.vue`],
+						defaultExtractor(content) {
+							const contentWithoutStyleBlocks = content.replace(
+								/<style[^]+?<\/style>/gi,
+								''
+							)
+							return (
+								contentWithoutStyleBlocks.match(
+									/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+								) || []
+							)
+						},
+						safelist: {
+							standard: [
+								/-(leave|enter|appear)(|-(to|from|active))$/,
+								/^(?!(|.*?:)cursor-move).+-move$/,
+								/^router-link(|-exact)-active$/,
+								/data-v-.*/,
+							],
+							deep: [/.*nprogress.*/],
+						},
+					}),
+					require('autoprefixer'),
+				],
+			},
+		},
+	},
+}
