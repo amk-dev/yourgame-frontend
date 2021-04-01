1c8652505efbf5fd32d8120b94cb24e1dbd1df89
amk-dev <akash.k.mohan98@gmail.com>
2021-04-01 17:54:08 +0530

    Enable PostCss only on production

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@
 
 const purgecss = require('@fullhuman/postcss-purgecss')
 
+const isProduction = process.env.NODE_ENV == 'production'
+
 module.exports = {
 	// configureWebpack: {
 	// 	plugins: [new BundleAnalyzerPlugin()],
@@ -10,32 +12,37 @@ module.exports = {
 	css: {
 		loaderOptions: {
 			postcss: {
-				plugins: [
-					purgecss({
-						content: [`./public/**/*.html`, `./src/**/*.vue`],
-						defaultExtractor(content) {
-							const contentWithoutStyleBlocks = content.replace(
-								/<style[^]+?<\/style>/gi,
-								''
-							)
-							return (
-								contentWithoutStyleBlocks.match(
-									/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
-								) || []
-							)
-						},
-						safelist: {
-							standard: [
-								/-(leave|enter|appear)(|-(to|from|active))$/,
-								/^(?!(|.*?:)cursor-move).+-move$/,
-								/^router-link(|-exact)-active$/,
-								/data-v-.*/,
-							],
-							deep: [/.*nprogress.*/],
-						},
-					}),
-					require('autoprefixer'),
-				],
+				plugins: isProduction
+					? [
+							purgecss({
+								content: [
+									`./public/**/*.html`,
+									`./src/**/*.vue`,
+								],
+								defaultExtractor(content) {
+									const contentWithoutStyleBlocks = content.replace(
+										/<style[^]+?<\/style>/gi,
+										''
+									)
+									return (
+										contentWithoutStyleBlocks.match(
+											/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+										) || []
+									)
+								},
+								safelist: {
+									standard: [
+										/-(leave|enter|appear)(|-(to|from|active))$/,
+										/^(?!(|.*?:)cursor-move).+-move$/,
+										/^router-link(|-exact)-active$/,
+										/data-v-.*/,
+									],
+									deep: [/.*nprogress.*/],
+								},
+							}),
+							require('autoprefixer'),
+					  ]
+					: [],
 			},
 		},
 	},
