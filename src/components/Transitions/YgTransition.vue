7c649ac66ff513661d545fd4bf391cbe6f5ac5ee
amk-dev <akash.k.mohan98@gmail.com>
2021-04-15 21:55:26 +0530

    update play view layout

--- src/components/Transitions/YgTransition.vue
+++ src/components/Transitions/YgTransition.vue
@@ -21,4 +21,16 @@
 	.fade-leave-to {
 		opacity: 0;
 	}
+
+	.appear-from-bottom-enter-active,
+	.appear-from-bottom-leave-active {
+		transition: transform 0.3s;
+		transition-timing-function: ease-in-out;
+		will-change: transform;
+	}
+
+	.appear-from-bottom-enter,
+	.appear-from-bottom-leave-to {
+		transform: translateY(400px);
+	}
 </style>
